223d2f61b8d1ffe66fb6524bbc4d7459865de9ae
Jax Leo <42597871+weaponmasterjax@users.noreply.github.com>
2025-01-13 05:53:33 +0000

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -6,7 +6,7 @@
 properties() { '
 kernel.string=Everpal Kernel: Adrenalin Edition by Weaponmasterjax
 do.devicecheck=0
-do.modules=0
+do.modules=1
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
